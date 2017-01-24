5c9a51ea5cbd55af0e66ae9a5acca85ae1e7a286
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-24 12:26:40 -0800

    Added logic to instantiate the new Texture View

--- app/src/main/java/ptcg/scharnitzke/com/ptcgapp/ScanQRCodeActivity.java
+++ app/src/main/java/ptcg/scharnitzke/com/ptcgapp/ScanQRCodeActivity.java
@@ -1,17 +1,28 @@
 package ptcg.scharnitzke.com.ptcgapp;
 
 import android.content.Context;
+import android.hardware.camera2.CameraCaptureSession;
+import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.TextureView;
 
 public class ScanQRCodeActivity extends AppCompatActivity {
+    private TextureView qrTextureView;
     private CameraManager cameraManager;
+    private String cameraId;
+
+    protected CameraDevice cameraDevice;
+    protected CameraCaptureSession cameraCaptureSession;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qrcode);
+
+        this.qrTextureView = (TextureView) findViewById(R.id.qrCameraTexture);
+        assert this.qrTextureView != null;
 
         this.cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
     }
