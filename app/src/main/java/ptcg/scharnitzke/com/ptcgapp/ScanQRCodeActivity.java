dc158b10242449ab1284d849c19f161dc1e8bb30
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-24 12:38:40 -0800

    Implemented a StateCallback for our camera device and moved the texture listener outside of onCreate

--- app/src/main/java/ptcg/scharnitzke/com/ptcgapp/ScanQRCodeActivity.java
+++ app/src/main/java/ptcg/scharnitzke/com/ptcgapp/ScanQRCodeActivity.java
@@ -5,11 +5,15 @@ import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.TextureView;
 
 public class ScanQRCodeActivity extends AppCompatActivity {
+    private static final String TAG = "AndroidCameraAPI";
+
     private TextureView qrTextureView;
     private CameraManager cameraManager;
     private String cameraId;
@@ -25,30 +29,47 @@ public class ScanQRCodeActivity extends AppCompatActivity {
         qrTextureView = (TextureView) findViewById(R.id.qrCameraTexture);
         assert qrTextureView != null;
 
-        TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
-            @Override
-            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
-                openCamera();
-            }
-
-            @Override
-            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
-            }
-
-            @Override
-            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
-                return false;
-            }
-
-            @Override
-            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
-
-            }
-        };
-
         cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
     }
+
+    TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+        @Override
+        public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+            openCamera();
+        }
+
+        @Override
+        public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+        }
+
+        @Override
+        public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+            return false;
+        }
+
+        @Override
+        public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+        }
+    };
+
+    private final CameraDevice.StateCallback stateCallback = new CameraDevice.StateCallback() {
+        @Override
+        public void onOpened(@NonNull CameraDevice camera) {
+            Log.e(TAG, "onOpened");
+        }
+
+        @Override
+        public void onDisconnected(@NonNull CameraDevice camera) {
+
+        }
+
+        @Override
+        public void onError(@NonNull CameraDevice camera, int error) {
+
+        }
+    };
 
     private void openCamera() {
 
