232bb677746a883e7d5901ba4115de8cac6c1d45
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-24 11:44:41 -0800

    Added a new Activity for scanning QR codes and modified the Scan button's click listener to launch it

--- app/src/main/java/ptcg/scharnitzke/com/ptcgapp/MainActivity.java
+++ app/src/main/java/ptcg/scharnitzke/com/ptcgapp/MainActivity.java
@@ -1,6 +1,7 @@
 package ptcg.scharnitzke.com.ptcgapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.support.v4.util.SparseArrayCompat;
@@ -17,6 +18,7 @@ import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 public class MainActivity extends Activity implements View.OnClickListener {
+    private static final int RC_BARCODE_CAPTURE = 9001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +32,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         if(v.getId() == R.id.scanQRButton) {
             // launch QR code scanner activity
+            Intent intent = new Intent(this, ScanQRCodeActivity.class);
+            startActivityForResult(intent, RC_BARCODE_CAPTURE);
         }
     }
 }
