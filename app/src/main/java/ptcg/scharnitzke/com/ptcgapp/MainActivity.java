6f85c4c49678ac42aea87c6112692aaf48a9f8ae
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-24 11:03:18 -0800

    Added logic to Scan button click handler to instantiate the Image View and decode the POC QR code into a bitmap

--- app/src/main/java/ptcg/scharnitzke/com/ptcgapp/MainActivity.java
+++ app/src/main/java/ptcg/scharnitzke/com/ptcgapp/MainActivity.java
@@ -1,9 +1,12 @@
 package ptcg.scharnitzke.com.ptcgapp;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -16,7 +19,11 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                ImageView myImageView = (ImageView) findViewById(R.id.imgView);
+                Bitmap myBitmap = BitmapFactory.decodeResource(
+                        getApplicationContext().getResources(),
+                        R.drawable.puppy
+                );
             }
         });
     }
