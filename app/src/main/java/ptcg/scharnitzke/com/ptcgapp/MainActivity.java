b1030407dd4809e74933ce33a133c87984b7aa78
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-24 11:01:11 -0800

    Gave the Scan button an ID and added a singleton click listener stub to it

--- app/src/main/java/ptcg/scharnitzke/com/ptcgapp/MainActivity.java
+++ app/src/main/java/ptcg/scharnitzke/com/ptcgapp/MainActivity.java
@@ -2,6 +2,8 @@ package ptcg.scharnitzke.com.ptcgapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(R.id.scanQRButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
