c1fdf31afc2f5bf4e5f31d7049061fbef25730cf
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-24 12:30:23 -0800

    Added a listener to open the camera when the texture is available and stubbed out the method to open the camera

--- app/src/main/java/ptcg/scharnitzke/com/ptcgapp/ScanQRCodeActivity.java
+++ app/src/main/java/ptcg/scharnitzke/com/ptcgapp/ScanQRCodeActivity.java
@@ -1,6 +1,7 @@
 package ptcg.scharnitzke.com.ptcgapp;
 
 import android.content.Context;
+import android.graphics.SurfaceTexture;
 import android.hardware.camera2.CameraCaptureSession;
 import android.hardware.camera2.CameraDevice;
 import android.hardware.camera2.CameraManager;
@@ -21,9 +22,35 @@ public class ScanQRCodeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qrcode);
 
-        this.qrTextureView = (TextureView) findViewById(R.id.qrCameraTexture);
-        assert this.qrTextureView != null;
+        qrTextureView = (TextureView) findViewById(R.id.qrCameraTexture);
+        assert qrTextureView != null;
 
-        this.cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+        TextureView.SurfaceTextureListener textureListener = new TextureView.SurfaceTextureListener() {
+            @Override
+            public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+                openCamera();
+            }
+
+            @Override
+            public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+
+            }
+
+            @Override
+            public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+                return false;
+            }
+
+            @Override
+            public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+
+            }
+        };
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
+    }
+
+    private void openCamera() {
+
     }
 }
