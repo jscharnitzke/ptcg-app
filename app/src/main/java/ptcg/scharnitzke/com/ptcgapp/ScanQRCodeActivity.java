5d708e119ec69a113ac299b47e324147e6a60587
Joe Scharnitzke <j.scharnitzke@gmail.com>
2017-01-24 12:04:35 -0800

    Added instantiation of CameraManager to the QR code activity and updated min SDK version to 21 since the camera2 API is not supported at lower SDK versions

--- app/src/main/java/ptcg/scharnitzke/com/ptcgapp/ScanQRCodeActivity.java
+++ app/src/main/java/ptcg/scharnitzke/com/ptcgapp/ScanQRCodeActivity.java
@@ -1,13 +1,18 @@
 package ptcg.scharnitzke.com.ptcgapp;
 
+import android.content.Context;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class ScanQRCodeActivity extends AppCompatActivity {
+    private CameraManager cameraManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scan_qrcode);
+
+        cameraManager = (CameraManager) getSystemService(Context.CAMERA_SERVICE);
     }
 }
